e9c6390db8995ce4742deac360ddf6816ae5f09f
michaelsimon <michael.simon@gmail.com>
2016-06-16 20:37:13 -0400

    add approved boolean field to user model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160610194640) do
+ActiveRecord::Schema.define(version: 20160616210700) do
 
   create_table "acts", force: :cascade do |t|
     t.string   "name"
@@ -85,12 +85,12 @@ ActiveRecord::Schema.define(version: 20160610194640) do
   add_index "performances", ["venue_id"], name: "index_performances_on_venue_id"
 
   create_table "users", force: :cascade do |t|
-    t.string   "email",                  default: "", null: false
-    t.string   "encrypted_password",     default: "", null: false
+    t.string   "email",                  default: "",    null: false
+    t.string   "encrypted_password",     default: "",    null: false
     t.string   "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
-    t.integer  "sign_in_count",          default: 0,  null: false
+    t.integer  "sign_in_count",          default: 0,     null: false
     t.datetime "current_sign_in_at"
     t.datetime "last_sign_in_at"
     t.string   "current_sign_in_ip"
@@ -101,8 +101,9 @@ ActiveRecord::Schema.define(version: 20160610194640) do
     t.integer  "role"
     t.integer  "venue_id"
     t.integer  "act_id"
-    t.datetime "created_at",                          null: false
-    t.datetime "updated_at",                          null: false
+    t.datetime "created_at",                             null: false
+    t.datetime "updated_at",                             null: false
+    t.boolean  "approved",               default: false, null: false
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
