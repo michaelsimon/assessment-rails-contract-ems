6c4faba79dd6f85e1f2099ea83efebba121455e8
michaelsimon <michael.simon@gmail.com>
2016-09-20 23:29:19 -0400

    initialize and work on programming AJAX post request for approval acceptance

---
+++ app/assets/javascripts/approvals.js
@@ -0,0 +1,18 @@
+
+
+$(document).ready(function() {
+  $('form').on('submit', function(event){
+    //code
+    event.preventDefault();
+    $('#approve').on("click", function(clicked){
+      $.ajax({
+        url: "/approvals/approve",
+        type: 'POST',
+        dataType: 'JSON',
+        data: {id: $(this)[0].dataset.id}
+      }).done(function(response, stat){
+        debugger;
+      });
+    });
+  });
+});
