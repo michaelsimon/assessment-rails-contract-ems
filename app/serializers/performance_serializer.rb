50dafa048e4bd1ad0bfd03d677c5441f676eca14
michaelsimon <michael.simon@gmail.com>
2016-09-22 21:47:18 -0400

    populate performance serializer with attributes and associations

--- app/serializers/performance_serializer.rb
+++ app/serializers/performance_serializer.rb
@@ -1,3 +1,6 @@
 class PerformanceSerializer < ActiveModel::Serializer
-  attributes :id
+  attributes :id, :act_id, :venue_id, :name, :perf_date, :perf_time, :tkts_url, :description
+  has_one :contract
+  has_one :venue
+  has_one :act
 end
