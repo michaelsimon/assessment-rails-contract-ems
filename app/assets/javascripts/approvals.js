1025edc323aa74e7e83c3efb69b61140a9ab6ad0
michaelsimon <michael.simon@gmail.com>
2016-09-22 21:03:46 -0400

    approve button sends jQ request to controller approve action

--- app/assets/javascripts/approvals.js
+++ app/assets/javascripts/approvals.js
@@ -1,18 +1,21 @@
 
 
 $(document).ready(function() {
-  $('form').on('submit', function(event){
+  $('form').on('click', function(event){
     //code
     event.preventDefault();
-    $('#approve').on("click", function(clicked){
+    // debugger;
+    if (event.currentTarget.id === "approve") {
       $.ajax({
         url: "/approvals/approve",
         type: 'POST',
         dataType: 'JSON',
-        data: {id: $(this)[0].dataset.id}
+        data: {id: $(this)[0][0].dataset.id}
       }).done(function(response, stat){
-        
+        $(`[data-approval-id="${response.id}"] > td#date_answered`)["0"].innerHTML = response.date_answered;
+        $(`[data-approval-id="${response.id}"] > td#status`)["0"].innerHTML = "approved";
+        $(`[data-approval-id="${response.id}"] > td#buttons`)["0"].innerHTML = "";
       });
-    });
+  }
   });
 });
