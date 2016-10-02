f49e9b879e2bd5741fdc5d3972e67f39768dac6c
michaelsimon <michael.simon@gmail.com>
2016-10-01 20:34:51 -0400

    refactor down to one ajax call from 3 potential

--- app/assets/javascripts/approvals.js
+++ app/assets/javascripts/approvals.js
@@ -1,38 +1,14 @@
 var approvalResponse = function(event){
   event.preventDefault();
-  answer = event.currentTarget.getAttribute("data-approval-answer")
-  if (event.currentTarget.getAttribute("data-approval-answer") === "approve") {
+  answer = event.currentTarget.getAttribute("data-approval-answer");
     $.ajax({
-      url: "/approvals/approve",
+      url: "/approvals/" + answer,
       type: 'POST',
       dataType: 'JSON',
       data: {id: event.currentTarget.getAttribute("data-approval-id")}
     }).done(function(response, stat){
-      $(`[data-approval-id="${response.id}"] > td#date_answered`)["0"].innerHTML = response.date_answered;
-      $(`[data-approval-id="${response.id}"] > td#status`)["0"].innerHTML = "approved";
-      $(`[data-approval-id="${response.id}"] > td#buttons`)["0"].innerHTML = "";
+      $(`[data-approval-id="${response.id}"] > td#date_answered`).text(response.date_answered);
+      $(`[data-approval-id="${response.id}"] > td#status`).text(response.status);
+      $(`[data-approval-id="${response.id}"] > td#buttons`).text("")
     });
-  } else if (event.currentTarget.getAttribute("data-approval-answer") === "reject") {
-    $.ajax({
-      url: "/approvals/reject",
-      type: 'POST',
-      dataType: 'JSON',
-      data: {id: event.currentTarget.getAttribute("data-approval-id")}
-    }).done(function(response, stat){
-      $(`[data-approval-id="${response.id}"] > td#date_answered`)["0"].innerHTML = response.date_answered;
-      $(`[data-approval-id="${response.id}"] > td#status`)["0"].innerHTML = "rejected";
-      $(`[data-approval-id="${response.id}"] > td#buttons`)["0"].innerHTML = "";
-    });
-  } else if (event.currentTarget.getAttribute("data-approval-answer") === "cancel") {
-    $.ajax({
-      url: "/approvals/cancel",
-      type: 'POST',
-      dataType: 'JSON',
-      data: {id: event.currentTarget.getAttribute("data-approval-id")}
-    }).done(function(response, stat){
-      $(`[data-approval-id="${response.id}"] > td#date_answered`)["0"].innerHTML = response.date_answered;
-      $(`[data-approval-id="${response.id}"] > td#status`)["0"].innerHTML = "canceled";
-      $(`[data-approval-id="${response.id}"] > td#buttons`)["0"].innerHTML = "";
-    });
-  }
 }
