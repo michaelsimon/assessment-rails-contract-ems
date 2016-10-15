e326889b79155946b14582b5a0e06e08af30209d
michaelsimon <michael.simon@gmail.com>
2016-10-15 16:01:11 -0400

    add gem 'jquery-turbolinks' to project to allow AJAX requests to trigger on page loads (which may already be cached)

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,6 +14,7 @@
 //= require jquery_ujs
 //= require bootstrap-sprockets
 //= require turbolinks
+//= require jquery.turbolinks
 //= require listeners
 //= require contract
 //= require performance
